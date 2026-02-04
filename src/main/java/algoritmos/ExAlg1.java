a8a0bec4296aec27286deef1af4d717b68c3966b
Pedro Pimentel <pedrohenriquepimenteldev05@gmail.com>
2026-02-04 06:28:44 -0300

    feat: Ex. Raio

---
+++ src/main/java/algoritmos/ExAlg1.java
@@ -0,0 +1,22 @@
+package algoritmos;
+
+import java.util.Scanner;
+
+/*Crie um programa que receba o raio de uma circunferência e calcule sua área.*/
+public class ExAlg1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        double raio,area;
+
+        System.out.print("Digite o raio da circuferência: ");
+        raio = sc.nextDouble();
+
+        area = Math.PI * Math.pow(raio,2);
+
+        System.out.printf("Resultado da área da circuferência: %.2f " , area);
+
+        sc.close();
+    }
+
+}
